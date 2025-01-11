d636077d9164a635ff0470ca22968a32821518d5
masud001 <madhnagar@gmail.com>
2025-01-12 03:36:34 +0600

    update footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -31,7 +31,7 @@ const Footer = () => {
             <h4 className='text-center'>Email</h4>
             <img src={locationIcons} className='img-fluid pb-2 mx-auto' width="50" height="50" alt="location" />
             <address className=' font-italic'>
-              <p className='m-0'>+880 1234 5678</p>
+              <p className='m-0'>example@gmail.com</p>
             </address>
           </div>
         </div>
